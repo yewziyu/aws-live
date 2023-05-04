42ddd05574dab177d65d8b734aaade087f9d30ae
yewziyu <132603992+yewziyu@users.noreply.github.com>
2023-05-05 01:30:23 +0800

    Add files via upload

---
+++ aws-live-main/config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cvudjj94qhzj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "AACS3253"
+customdb = "employee"
+custombucket = "yewziyu-employee"
+customregion = "us-east-1"
+
